4fdc8c0b88e1e4392f91afd3e2f5a2489db23103
“Nguirane120” <badaranguirane120@gmail.com>
2024-09-01 12:56:39 +0000

    *

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoginGuard } from './guard/login.guard';
+import { AuthGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: AuthComponent, canActivate: [LoginGuard] },
 ];
